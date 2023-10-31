bf3a0627ac3583cc1acea92fe4cb5f949cb8a635
Revathi58-24 <118613279+Revathi58-24@users.noreply.github.com>
2023-10-31 21:09:16 +0530

    Update Signin.js

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -19,7 +19,7 @@ const SignIn = () => {
      // alert('Sign-in successful!');
       console.log('Continue with sign-in for User ID:', userID);
   
-      Axios.post('https://bankserve.onrender.com/api/users', { userID })
+      Axios.post('https://bankserver1.vercel.app/api/users', { userID })
         .then(response => {
           console.log(response.data.message);
           alert(response.data.message); // Display success message
